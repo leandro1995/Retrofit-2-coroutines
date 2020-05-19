7feb4dd193948f16972353ff38bfdb596569fdcb
Leandro Castillo Borja <leccbo1995@gmail.com>
2020-05-18 23:51:30 -0500

    Implementar respuesta del servicio
    Se coloco implementacion de java 1.8 y modificar call por response

--- app/src/main/java/com/leandro1995/retrofit2coroutines/MainActivity.kt
+++ app/src/main/java/com/leandro1995/retrofit2coroutines/MainActivity.kt
@@ -2,11 +2,33 @@ package com.leandro1995.retrofit2coroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.leandro1995.retrofit2coroutines.retrofit.config.RetrofitConfig
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        GlobalScope.launch(Dispatchers.Main) {
+            try {
+                RetrofitConfig.apiService.userList().let {
+                    if (it.isSuccessful) {
+
+                    }
+                }
+            } catch (ex: Exception) {
+                Toast.makeText(
+                    this@MainActivity,
+                    getString(R.string.without_internet),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
     }
 }
--- app/src/main/java/com/leandro1995/retrofit2coroutines/retrofit/ApiService.kt
+++ app/src/main/java/com/leandro1995/retrofit2coroutines/retrofit/ApiService.kt
@@ -1,11 +1,11 @@
 package com.leandro1995.retrofit2coroutines.retrofit
 
 import com.google.gson.JsonArray
-import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 
 interface ApiService {
 
     @GET("posts")
-    suspend fun userList() : Call<JsonArray>
+    suspend fun userList(): Response<JsonArray>
 }
